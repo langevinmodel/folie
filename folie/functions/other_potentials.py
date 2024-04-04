cdf5a58672ffe18eed04a2372fde09bd9eafef56
DaniB <daniele.bersano9@gmail.com>
2024-04-04 19:26:30 +0200

    cration of file 'other potentials.py' in order not to modify anaytical for the time being

--- folie/functions/other_potentials.py
+++ folie/functions/other_potentials.py
@@ -1,10 +1,10 @@
-# Sme import as analytical.py
+# Same import as analytical.py
 from .base import Function
 from .._numpy import np
 from ..domains import Domain
 from .analytical import PotentialFunction
 
-class ExamplePotential(PotentialFunction):
+class My_Quartic2D(PotentialFunction):
      """
      An example of potential
      """
@@ -25,6 +25,8 @@ class ExamplePotential(PotentialFunction):
 
      # This is a convenience function for user, implementation is optionnal
      def potential(self, x , y ): # requires an np.mesh as input 
-         return 2*x**2 - x*y + 2*y**2
-     def force(self, X): #This is -grad(potential).  That should return an array of size (X.shape[0], dim).
-         return self.a * np.ones_like(X)
+         return 2*x**4 - 4*x**2 + 2*y**2
+     def force_x_comp(self, x): #This is -grad(potential).  That should return an array of size (X.shape[0], dim).
+         return -8*x**3+8*x
+     def force_y_comp(self, y): #This is -grad(potential).  That should return an array of size (X.shape[0], dim).
+         return -4*y
