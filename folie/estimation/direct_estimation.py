4e911c763cbd58e516803b9204c79f489808ec09
Hadrien <hadrien@langevin>
2024-06-04 09:22:17 +0200

    Base of underdamped estimation

--- folie/estimation/direct_estimation.py
+++ folie/estimation/direct_estimation.py
@@ -96,3 +96,93 @@ class KramersMoyalEstimator(Estimator):
         self.model.diffusion.fit(X, y=dx_sq / dt, estimator=estimator, **kwargs)
         self.model.fitted_ = True
         return self
+
+
+class UnderdampedKramersMoyalEstimator(KramersMoyalEstimator):
+    r"""Obtain underdamped model. It's a biased estimator that does not yield correct results but still provide interesting starting point for optimisation
+
+    Parameters
+    ----------
+    model : Model, optional, default=None
+        A model which can be used for initialization. In case an estimator is capable of online learning, i.e.,
+        capable of updating models, this can be used to resume the estimation process.
+    """
+
+    def __init__(self, model):
+        super().__init__(model)
+
+    def fit(self, data, **kwargs):
+        r"""Fits data to the estimator's internal :class:`Model` and overwrites it. This way, every call to
+        :meth:`fetch_model` yields an autonomous model instance. Sometimes a :code:`partial_fit` method is available,
+        in which case the model can get updated by the estimator.
+
+        Parameters
+        ----------
+        data : array_like
+            Data that is used to fit a model.
+        **kwargs
+            Additional kwargs.
+
+        Returns
+        -------
+        self : Estimator
+            Reference to self.
+        """
+
+        for trj in data:
+            self.preprocess_traj(trj)
+
+        dt = data[0]["dt"]
+
+        X = np.concatenate([trj["x"] for trj in data], axis=0)
+        V = np.concatenate([trj["v"] for trj in data], axis=0)
+        bias = np.concatenate([trj["bias"] for trj in data], axis=0)
+        extra_kwargs = {}
+        for key in ["cells_idx", "loc_x"]:
+            if key in data[0]:
+                extra_kwargs[key] = np.concatenate([trj[key] for trj in data], axis=0)
+
+        # weights = np.concatenate(data.weights, axis=0)  # TODO: implement correctly the weights
+
+        # Take weight into account as well
+        dim = X.shape[1]
+        acc = np.concatenate([trj["a"] for trj in data], axis=0)
+        if dim <= 1:
+            acc = acc.ravel()
+        self.model.meandispl.fit(X, V, bias, y=acc, sample_weight=None)
+        acc -= self.model.force(X)
+        if dim <= 1:
+            acc_sq = acc ** 2
+        else:
+            acc_sq = acc[..., None] * acc[:, None, ...]
+        self.model.diffusion.fit(X, y=acc_sq * dt)
+        self.model.fitted_ = True
+
+        return self
+
+    def preprocess_traj(self, trj, **kwargs):
+        """
+        Compute velocity and acceleration
+        """
+
+        if "a" not in list(trj.keys()):
+            diffs = trj["x"] - np.roll(trj["x"], 1, axis=0)
+            a = np.roll(diffs, -1, axis=0) - diffs
+            if "v" not in list(trj.keys()):
+                trj["v"] = (0.5 / trj["dt"]) * (trj["x"] - np.roll(trj["x"], 2, axis=0))[2:-1]
+            else:
+                trj["v"] = trj["v"][2:-1]
+            trj["a"] = a[2:-1] / (trj["dt"] ** 2)
+            trj["x"] = trj["x"][2:-1]
+            if "bias" in trj:
+                trj["bias"] = trj["bias"][2:-1]
+            else:
+                trj["bias"] = np.zeros((1, trj["x"].shape[1]))
+            if hasattr(self._model, "dim_h"):
+                if self._model.dim_h > 0:
+                    trj["sig_h"] = np.zeros((trj["x"].shape[0], 2 * self._model.dim_h, 2 * self._model.dim_h))
+                    trj["v"] = np.concatenate((trj["v"], np.zeros((trj["v"].shape[0], self._model.dim_h))), axis=1)
+                    trj["a"] = np.concatenate((trj["a"], np.zeros((trj["a"].shape[0], self._model.dim_h))), axis=1)
+                    trj["bias"] = np.concatenate((trj["bias"], np.zeros((trj["bias"].shape[0], self._model.dim_h))), axis=1)
+            self._model.preprocess_traj(trj, **kwargs)
+        return trj
